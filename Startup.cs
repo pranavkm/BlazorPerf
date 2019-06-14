41ad0bf19e2d88de4b8573b7cc4d3532735b645d
Pranav K <prkrishn@hotmail.com>
2019-06-14 11:47:19 -0700

    Remove HSTS

--- Startup.cs
+++ Startup.cs
@@ -40,11 +40,8 @@ namespace BlazorServerSIde
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
